d9e04b99da31173faa80688752a07fce25dc7462
praneetha2005 <123438629+praneetha2005@users.noreply.github.com>
2023-03-29 19:27:00 +0530

    Add files via upload

---
+++ leap.cpp
@@ -0,0 +1,13 @@
+#include<conio.h>
+#include<stdio.h>
+int main()
+{
+	int n,i;
+	printf("enter the year:");
+	scanf("%d",&n);
+	if(i%4==0)
+	printf("the given year is leap year");
+	else
+	printf("the given year is not leap year");
+return 0;
+}
